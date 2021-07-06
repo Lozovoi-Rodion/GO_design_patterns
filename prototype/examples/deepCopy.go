bf960bcb5eeeab6b51814f348bbfd1c622efff9a
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-06 16:15:44 +0300

    deep copy method

--- prototype/examples/deepCopy.go
+++ prototype/examples/deepCopy.go
@@ -6,18 +6,42 @@ type Address struct {
 	StreetAddress, City, Country string
 }
 
+func (a *Address) DeepCopy() *Address {
+	return &Address{a.StreetAddress, a.City, a.Country}
+}
+
 type Person struct {
-	Name string
+	Name    string
 	Address *Address
+	Friends []string
+}
+
+func (p *Person) DeepCopy() *Person {
+	q := *p
+	q.Address = p.Address.DeepCopy()
+	copy(q.Friends, p.Friends)
+	return &q
 }
 
 // DeepCopyExample can not be scaled
-func DeepCopyExample()  {
-	john := Person{"John", &Address{"Johnny Avenue 13", "London", "UK"}}
+func DeepCopyExample() {
+	john := Person{
+		"John",
+		&Address{"Johnny Avenue 13", "London", "UK"},
+		[]string{"Jim", "Bill"},
+	}
+
 	jane := john
 	jane.Name = "Jane"
-	jane.Address.StreetAddress = "321 Baker St"
+	// INCORRECT:
+	//jane.Address.StreetAddress = "321 Baker St"
+	//
+	//fmt.Println(john.Name, john.Address)
+	//fmt.Println(jane.Name, jane. Address)
+
+	jane.Address = &Address{john.Address.StreetAddress, john.Address.City, john.Address.Country}
+	jane.Address.StreetAddress = "East West Coast 33"
 
 	fmt.Println(john.Name, john.Address)
-	fmt.Println(jane.Name, jane. Address)
+	fmt.Println(jane.Name, jane.Address)
 }
--- prototype/main.go
+++ prototype/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Lozovoi-Rodion/GO_design_patterns/prototype/examples"
+import (
+	"fmt"
+	"github.com/Lozovoi-Rodion/GO_design_patterns/prototype/examples"
+)
 
-func main()  {
-	b := prototype.DeepCopyExample()
+func main() {
+	prototype.DeepCopyExample()
+
+	john := prototype.Person{"John", &prototype.Address{"Starway Road 32", "Bejing", "China"}, []string{"Li", "Lu"}}
+	jane := john.DeepCopy()
+	jane.Name = "Jane"
+	jane.Address.StreetAddress = "321 Baker St"
+	jane.Friends = append(jane.Friends, "Kuan")
+
+	fmt.Println("---!!!!---")
+	fmt.Println(john.Name, john.Address, john.Friends)
+	fmt.Println(jane.Name, john.Address, jane.Friends)
 }
