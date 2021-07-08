800d6339f28b99fec585c32fd4b8189b674a02fb
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-08 22:10:19 +0300

    finished composite, started decorator

---
+++ decorator/go.mod
@@ -0,0 +1,3 @@
+module github.com/Lozovoi-Rodion/GO_design_patterns/decorator
+
+go 1.16
---
+++ decorator/main.go
@@ -0,0 +1,13 @@
+package main
+
+
+
+// - used to augment an object with additional functionality
+// - do not want to rewrite or alter existing code (OCP)
+// - want to keep new functionality separate (SRP)
+// - need to be able to interact with existing structs
+// Decorator - is a dp that facilitates the addition of behaviors to individual objects through embedding
+
+func main() {
+
+}
