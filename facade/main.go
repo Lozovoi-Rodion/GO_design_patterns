2b271d9b424888820eca458b4d138ad880780840
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-09 13:20:38 +0300

    finished facade, started flyweight

--- facade/main.go
+++ facade/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "github.com/Lozovoi-Rodion/GO_design_patterns/facade/examples"
+import (
+	"fmt"
+	"github.com/Lozovoi-Rodion/GO_design_patterns/facade/examples"
+)
 // Facade provides a simple, easy to understand/user interface over
 // a large and sophisticated body of code
-
+// Summary:
+// - facade provides simplified API over a set of components
+// - may wish to expose (optionally) internals through the facade
+// - may allow users to use more complex API if they need to
 
 func main()  {
 	c := facade.NewConsole()
 	u := c.GetCharacterAt(1)
+	fmt.Println(u)
 }
