5a4f2f5a9a9f96f5897953c428f6557517489546
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-14 21:09:17 +0300

    lexing done

--- interpreter/main.go
+++ interpreter/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main()  {
-	
+// examples:
+// Programming language compilers, interpreters and IDEs
+// HTML, XML and similar
+// Numeric expressions (3+4/5)
+// reg.exps
+// Interpreter - component that process structured data. Does so by
+// turning it into separated lexical tokens (lexing) and
+// then interpreting sequences of said tokens (parsing)
+func main() {
+	input := "(13+4)-(12+1)"
+	tokens := interpreter.Lex(input)
 }
