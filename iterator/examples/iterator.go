360e7ec2af2220e9ea085214da0dc69df642954e
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-19 15:53:23 +0300

    working on iterator

--- iterator/examples/iterator.go
+++ iterator/examples/iterator.go
@@ -1,1 +1,9 @@
-package examples
+package iterator
+
+type Person struct {
+	FirstName, MiddleName, LastName string
+}
+
+func (p *Person) Names() [3]string {
+	return [3]string{p.FirstName, p.MiddleName, p.LastName}
+}
--- iterator/main.go
+++ iterator/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/Lozovoi-Rodion/GO_design_patterns/iterator/examples"
 
+func main() {
+	p := iterator.Person{"Alexander", "Graham", "Bell"}
 }
