26802dc1d096becdd4144c11c53fed9c0eb16651
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-20 14:07:35 +0300

    finished iterator, started observer

--- iterator/examples/iterator.go
+++ iterator/examples/iterator.go
@@ -4,6 +4,46 @@ type Person struct {
 	FirstName, MiddleName, LastName string
 }
 
+type PersonNameIterator struct {
+	person  *Person
+	current int
+}
+
+func NewPersonNameIterator(person *Person) *PersonNameIterator {
+	return &PersonNameIterator{person, -1}
+}
+
+func (p *PersonNameIterator) MoveNext() bool {
+	p.current++
+	return p.current < 3
+}
+
+func (p *PersonNameIterator) Value() string {
+	switch p.current {
+	case 0:
+		return p.person.FirstName
+	case 1:
+		return p.person.MiddleName
+	case 2:
+		return p.person.LastName
+	default:
+		panic("Not known value")
+	}
+}
+
 func (p *Person) Names() [3]string {
 	return [3]string{p.FirstName, p.MiddleName, p.LastName}
+}
+
+func (p *Person) NamesGenerator() <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		out <- p.FirstName
+		if len(p.MiddleName) > 0 {
+			out <- p.MiddleName
+		}
+		out <- p.LastName
+	}()
+	return out
 }
--- iterator/main.go
+++ iterator/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/Lozovoi-Rodion/GO_design_patterns/iterator/examples"
+import (
+	"fmt"
+	iterator "github.com/Lozovoi-Rodion/GO_design_patterns/iterator/examples"
+)
+
+// Summary:
+// - an iterator specifies how can you traverse an object
+// - moves along the iterated collection, indicating when last element has been reached
+// - not idiomatic in go
 
 func main() {
 	p := iterator.Person{"Alexander", "Graham", "Bell"}
+	for _, name := range p.Names() {
+		fmt.Println(name)
+	}
+
+	p2 := iterator.Person{"Nikola", "", "Tesla"}
+	for name := range p2.NamesGenerator() {
+		fmt.Println(name)
+	}
+
+	for it := iterator.NewPersonNameIterator(&p); it.MoveNext(); {
+		fmt.Println(it.Value())
+	}
+
+	root := iterator.NewNode(1, iterator.NewNode(2, iterator.NewTerminalNode(21), iterator.NewTerminalNode(22)), iterator.NewTerminalNode(3))
+	it := iterator.NewInOrderIterator(root)
+
+	for it.MoveNext() {
+		fmt.Printf("%d", it.Current.Value)
+	}
+	fmt.Printf("\n")
+
+	t := iterator.NewBinaryTree(root)
+	for i := t.InOrder(); i.MoveNext(); {
+		fmt.Printf("%d,", i.Current.Value)
+	}
+	fmt.Printf("\n")
 }
