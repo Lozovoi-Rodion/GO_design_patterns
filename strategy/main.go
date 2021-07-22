0e809c6f5681ccbd339f50e5e5b4927b9747edca
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-07-22 12:16:40 +0300

    finished strategy, starting visitor

--- strategy/main.go
+++ strategy/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Lozovoi-Rodion/GO_design_patterns/strategy/examples"
+)
 
+// Strategy - separates an algorithm into its `skeleton` and concrete implementation
+// steps, which can be varied at run-time
+// Summary:
+// - define an algorithm at a high level
+// - define the interface you expect strategy to follow
+// - support the injection of the strategy into the high-level algorithm
+
+func main() {
+	tp := strategy.NewTextProcessor(&strategy.MarkdownListStrategy{})
+	tp.AppendList([]string{"This", "is", "markdown", "list"})
+	fmt.Println(tp.String())
+
+	tp.Reset()
+	tp.SetOutputFormat(strategy.Html)
+	tp.AppendList([]string{"This", "is", "html", "list"})
+	fmt.Println(tp.String())
 }
---
+++ visitor/go.mod
@@ -0,0 +1,3 @@
+module github.com/Lozovoi-Rodion/GO_design_patterns/visitor
+
+go 1.16
