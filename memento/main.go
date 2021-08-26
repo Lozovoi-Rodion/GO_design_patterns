7c9a8521aed1e96fb6cd003f1240cc9a23cc795c
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-08-26 18:13:44 +0300

    added memento

--- memento/main.go
+++ memento/main.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Lozovoi-Rodion/GO_design_patterns/memento/examples"
+)
+
 // Memento - is a behavioral dp that lets you save and restore the previous state of an object
 // without revealing the details of its implementation
 
-func main()  {
+func main() {
+	ba, m := memento.NewBankAccount(100)
 
+	m1 := ba.Deposit(50)
+	fmt.Println(m1)
+	fmt.Println(ba)
+	ba.Restore(m)
+	fmt.Println(ba)
 }
