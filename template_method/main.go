87888a169313ea5633f61139fc3f5b4cd3478c70
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-09-14 17:02:43 +0300

    added template impl

--- template_method/main.go
+++ template_method/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main()  {
+// Overview:
+// algorithms can be decomposed into common parts + specifics
+// Strategy pattern does this through composition:
+// - high-level algo uses an interface
+// - concrete implementation implements the interface
+// - we keep a pointer to the interface; provide concrete implementation
+// Template Method performs a similar operation, but:
+// it's typically a function, not a struct with a reference to the implementation
+// can still use interfaces (just like Strategy): or can be functional (takes several fns as params)
+// Template Method - is a dp where a skeleton defined in a function. Function can either use an interface (like Strategy)
+// or can take several fns as args.
 
+func main() {
+	chess := template_method.NewGameOfChess()
+	template_method.PlayGame(chess)
 }
